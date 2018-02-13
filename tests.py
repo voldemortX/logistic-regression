f4bdc30e5a01d556d3de9b5a65eb8416fac4b05a
voldemort <32259501+voldemortX@users.noreply.github.com>
2018-02-13 14:24:52 +0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
 from reviewModel import *
 
 #calculateWordList();
-reviewModel(num_iterations=2000, learning_rate=0.005, print_cost=True);
+reviewModel(num_iterations=1000, learning_rate=0.01, print_cost=True);
